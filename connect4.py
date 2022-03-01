4044c68a4032ee0366d29be9a117c01df53e02e8
Paras M <64939659+pmxi@users.noreply.github.com>
2022-03-01 08:44:58 -0500

    added function annotations

--- connect4.py
+++ connect4.py
@@ -1,10 +1,10 @@
-def render(l):
+def render(l) -> None:
     for y in l:
         for x in y:
             print(x, end=" ")
         print()
 
-def move(l, move, player):
+def move(l, move, player) -> list:
     options = [x[move] for x in l]
     print("options", options)
     movey = options.index(0)
@@ -14,6 +14,9 @@ def move(l, move, player):
         spot +=1
     l[spot][move] = player
     return l
+
+# def check_win(l) -> bool:
+
 
 board = [[0] * 7 for i in range(6)]
 
