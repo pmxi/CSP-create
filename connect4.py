6767799d08c5771f786450e78a09a590d6d041c4
Paras M <64939659+pmxi@users.noreply.github.com>
2022-03-01 08:40:14 -0500

    switch players 1, -1

--- connect4.py
+++ connect4.py
@@ -4,7 +4,7 @@ def render(l):
             print(x, end=" ")
         print()
 
-def move(l, move):
+def move(l, move, player):
     options = [x[move] for x in l]
     print("options", options)
     movey = options.index(0)
@@ -12,15 +12,17 @@ def move(l, move):
     spot = 0
     while l[spot][move] != 0:
         spot +=1
-    l[spot][move] = 1
+    l[spot][move] = player
     return l
 
 board = [[0] * 7 for i in range(6)]
 
 play = True
+player = 1
 
 while play:
     movecol = int(input("column"))
     render(board)
-    l = move(board, movecol)
+    l = move(board, movecol, player)
     render(l)
+    player *= -1
