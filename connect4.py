6ac28cf8957642306370d0481fdcecdc7b7eaea0
Paras M <64939659+pmxi@users.noreply.github.com>
2022-03-01 21:49:41 -0500

    gitignore

--- connect4.py
+++ connect4.py
@@ -4,6 +4,7 @@ def render(l) -> None:
             print(x, end=" ")
         print()
 
+
 def move(l, move, player) -> list:
     options = [x[move] for x in l]
     print("options", options)
@@ -11,11 +12,31 @@ def move(l, move, player) -> list:
     print("movey", movey)
     spot = 0
     while l[spot][move] != 0:
-        spot +=1
+        spot += 1
     l[spot][move] = player
     return l
 
-# def check_win(l) -> bool:
+
+def check_win(l, player) -> bool:
+    # horizontal check
+    count = 0
+    for horizontal in l:
+        for piece in horizontal:
+            if piece == player:
+                count += 1
+                if count >= 4:
+                    return True
+            else:
+                count = 0
+    for vertical in range(5):
+        for piece in horizontal:
+            if piece == player:
+                count += 1
+                if count >= 4:
+                    return True
+            else:
+                count = 0
+    return False
 
 
 board = [[0] * 7 for i in range(6)]
@@ -28,4 +49,5 @@ while play:
     render(board)
     l = move(board, movecol, player)
     render(l)
+    print(check_win(board, player))
     player *= -1
