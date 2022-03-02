e5687ee8be95184db571f8b10ac3c15d1f9e2d42
Paras M <64939659+pmxi@users.noreply.github.com>
2022-03-02 09:47:47 -0500

    sdlfkj

--- connect4.py
+++ connect4.py
@@ -1,44 +1,91 @@
-def render(l) -> None:
-    for y in l:
+def render():
+    global board
+    for y in board:
         for x in y:
             print(x, end=" ")
         print()
 
 
-def move(l, move, player) -> list:
-    options = [x[move] for x in l]
+def move(move: int, player: int) -> int:
+    global board
+    options = [x[move] for x in board]
     print("options", options)
     movey = options.index(0)
     print("movey", movey)
     spot = 0
-    while l[spot][move] != 0:
+    while board[spot][move] != 0:
         spot += 1
-    l[spot][move] = player
-    return l
+    board[spot][move] = player
+    return spot
 
 
-def check_win(l, player) -> bool:
-    # horizontal check
-    count = 0
-    for horizontal in l:
-        for piece in horizontal:
-            if piece == player:
-                count += 1
-                if count >= 4:
-                    return True
-            else:
-                count = 0
-    for vertical in range(5):
-        for piece in horizontal:
-            if piece == player:
-                count += 1
-                if count >= 4:
-                    return True
-            else:
-                count = 0
-    return False
+# def check_win(l: list, player: int) -> bool:
+#     # horizontal check
+#     count = 0
+#     for horizontal in l:
+#         for piece in horizontal:
+#             if piece == player:
+#                 count += 1
+#                 if count >= 4:
+#                     return True
+#             else:
+#                 count = 0
+#     for vertical in range(5):
+#         for piece in horizontal:
+#             if piece == player:
+#                 count += 1
+#                 if count >= 4:
+#                     return True
+#             else:
+#                 count = 0
+#     return False
 
-
+# check win around a new move because that is the only place a new win can occur
+def check_win_move(movex: int, movey: int, player: int) -> bool:
+    global board
+    counter = 0
+    #horizontal win
+    for x in range(movex-3,movex+4):
+        spot = board[movex][movey]
+        try:
+            spot = board[movex][movey]
+        except IndexError:
+            pass
+        if board[movey][x] == player:
+            counter += 1
+        else:
+            counter = 0
+        if counter > 3:
+            return True
+    #vertical win
+    for y in range(movey-3,movey+4):
+        if board[y][movex] == player:
+            counter += 1
+        else:
+            counter = 0
+        if counter > 3:
+            return True
+    #diagonal 1 down
+    y = movey-3
+    for x in range(movex-3,movex+4):
+        if board[x][y] == player:
+            counter += 1
+        else:
+            counter = 0
+        if counter > 3:
+            return True
+    y += 1
+    #diagonal 2 up
+    y = movey-3
+    for x in range(movex-3,movex+4):
+        if board[x][y] == player:
+            counter += 1
+        else:
+            counter = 0
+        if counter > 3:
+            return True
+    y += 1
+    
 board = [[0] * 7 for i in range(6)]
 
 play = True
@@ -46,8 +93,8 @@ player = 1
 
 while play:
     movecol = int(input("column"))
+    render()
+    y = move(movecol, player)
     render(board)
-    l = move(board, movecol, player)
-    render(l)
-    print(check_win(board, player))
+    print(check_win_move(player))
     player *= -1
